52162719376d852e5de8d3a5489460e39314e87c
HArnao <harnaol1@upao.edu.pe>
2023-10-13 20:04:54 -0500

    Commit inicial

---
+++ HealthFuel/src/main/java/com/upao/healthfuelapi/HealthFuelApplication.java
@@ -0,0 +1,13 @@
+package com.upao.healthfuelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthFuelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthFuelApplication.class, args);
+    }
+
+}
---
+++ HealthFuel/src/test/java/com/upao/healthfuelapi/HealthFuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.healthfuelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthFuelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
