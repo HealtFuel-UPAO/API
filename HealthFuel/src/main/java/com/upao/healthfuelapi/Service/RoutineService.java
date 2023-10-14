6aa66836bfefe9e6db6543e05faa460f0e51ed09
ramdhum <ramdhun2018@gmail.com>
2023-10-14 03:47:10 -0500

    HU: Visualizar Rutina

--- HealthFuel/src/main/java/com/upao/healthfuelapi/Service/RoutineService.java
+++ HealthFuel/src/main/java/com/upao/healthfuelapi/Service/RoutineService.java
@@ -1,10 +1,13 @@
 package com.upao.healthfuelapi.Service;
 
 import com.upao.healthfuelapi.model.Routine;
+import java.util.List;
 
 public interface RoutineService {
 
     Routine addRoutine(Routine routine);
+
+    List<Routine> findAllRoutine();
 
 }
 
